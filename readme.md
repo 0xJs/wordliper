1c7c72ecced9eb5e8a17ae1568e38df06fc10b96
kadeeli <me@jonyschats.nl>
2020-03-23 04:48:00 -0400

    Added more functionalities

--- readme.md
+++ readme.md
@@ -14,11 +14,13 @@ python3 wordliper.py
 
 ### Preview
 ```
-usage: wordliper.py [-h] [-s STRINGS [STRINGS ...]] [-m] [-p]
-                    [-aY YEARS [YEARS ...]] [-AS]
+kadeeli@Pentest:/opt/wordliper$ ./wordliper.py 
+
+usage: wordliper.py [-h] [-f FILE] [-s STRINGS [STRINGS ...]] [-m] [-se]
+                    [-dutch] [-aY YEARS [YEARS ...]] [-AS]
                     [-As ASTRINGS [ASTRINGS ...]] [-PS]
                     [-Ps PSTRINGS [PSTRINGS ...]] [-C] [-min MIN] [-max MAX]
-                    [-o OUTPUT]
+                    [-minc] [-o OUTPUT]
 
 A simple wordlist generator to generate worldlists including custom strings
 such as company name, locations, months, periods and appending years,
@@ -26,44 +28,68 @@ capitalizing first letters. Basicly permutating the wordlist.
 
 optional arguments:
   -h, --help            show this help message and exit
+  -f FILE, --file FILE  used to supply a wordlist file as input, every word
+                        has to be a newline
   -s STRINGS [STRINGS ...], --strings STRINGS [STRINGS ...]
                         used to supply a(or multiple) strings such as company
-                        name(s), locations etc, seperated by spaces to be
-                        added to the wordlist
+                        name(s), locations etc. to be added to the wordlist.
+                        These need to be seperated by spaces
   -m, --months          used to add all months of the year to the worldlist
-  -p, --periods         used to add all periods of the year to the worldlist,
-                        like spring etc
+  -se, --seasons        used to add all season to the wordlist
+  -dutch, --dutch       used to translate months and seasons to dutch
   -aY YEARS [YEARS ...], --years YEARS [YEARS ...]
-                        used to append a year range to every word in the
-                        wordlist
-  -AS, --asymbols       used to append symbols to every word in the wordlist
+                        used to append a range of years to every word in the
+                        wordlist, seperated by spaces for example 2018 2020
+  -AS, --asymbols       used to append symbols to every word in the wordlist,
+                        the list of symbols can be edited in the code
   -As ASTRINGS [ASTRINGS ...], --astrings ASTRINGS [ASTRINGS ...]
                         used to append strings to every word in the wordlist
                         before the symbols are applied
-  -PS, --psymbols       used to prepend symbols to every word in the wordlist
+  -PS, --psymbols       used to prepend symbols to every word in the wordlist,
+                        the list of symbols can be edited in the code
   -Ps PSTRINGS [PSTRINGS ...], --pstrings PSTRINGS [PSTRINGS ...]
                         used to prepend strings to every word in the wordlist
                         before the symbols are applied
   -C, --capital         used to capitalize every first letter in the word
-  -min MIN, --min MIN   used to set the minimum password length
-  -max MAX, --max MAX   used to set the maximum password length
+  -min MIN, --min MIN   used to set the minimum password length of the
+                        outputted passwords
+  -max MAX, --max MAX   used to set the maximum password length of the
+                        outputted passwords
+  -minc, --minc         Set password requirement to have atleast a capital
+                        letter, removes every word that does not
   -o OUTPUT, --output OUTPUT
                         Directs the output to a name of your choice
 ```
 
 **EXAMPLE**
 ```
-sudo ./wordliper.py -s kadeeli -m -p -aY 2019 2020 -AS -PS -C -min 8 -max 16 -o example.txt
+kadeeli@Pentest:/opt/wordliper$ sudo ./wordliper.py -f shortwordlist.txt -m -se -dutch -aY 2019 2020 -AS -C --min 6 --max 16 -minc -o newwordlist.txt
 
+[+] Translating months and seasons to dutch variants
+[+] Adding words from shortwordlist.txt to the wordlist
 [+] Adding months to the worldlist
-[+] Adding periods to the worldlist
-[+] Adding the provided strings to the worldlist
+[+] Adding seasons to the worldlist
 [+] Appending years to every word in the wordlist
 [+] Appending symbols to every word in the wordlist
-[+] Prepending symbols to every word in the wordlist
 [+] Capitalizing the first letter of every word
-[-] Deleting words if smaller then 8 characters and larger then 16 characters
-[=] Generated 2118 words
+[-] Deleting words if smaller then 6 characters and larger then 16 characters
+[-] Deleting words that do not contain a capital character
+[=] Generated 702 words
+
+--output example--
+kadeeli@Pentest:/opt/wordliper$ tail -n 10 newwordlist.txt 
+
+kadeeli@Pentest:/opt/wordliper$ tail -n 10 newwordlist.txt 
+Zomer2020.
+Zomer2020/
+Zomer2020123
+Zomer20201234
+Zomer2020?
+Zomer2020@
+Zomer2020_
+Zomer?
+Zomer@
+Zomer_
 
 ```
 
--- wordliper.py
+++ wordliper.py
@@ -6,32 +6,34 @@ import sys
 
 ##PARAMETERS##
 parser = argparse.ArgumentParser(description='A simple wordlist generator to generate worldlists including custom strings such as company name, locations, months, periods and appending years, capitalizing first letters. Basicly permutating the wordlist.')
+parser.add_argument('-f', '--file', action='store', type=str,
+                    help='used to supply a wordlist file as input, every word has to be a newline')
 parser.add_argument('-s', '--strings', nargs='+', action='store', type=str, default=[], 
-                    help='used to supply a(or multiple) strings such as company name(s), locations etc, seperated by spaces to be added to the wordlist')
+                    help='used to supply a(or multiple) strings such as company name(s), locations etc. to be added to the wordlist. These need to be seperated by spaces')
 parser.add_argument('-m', '--months', action='store_true', 
                     help='used to add all months of the year to the worldlist')
 parser.add_argument('-se', '--seasons', action='store_true',
-                    help='used to add all seaons of the year to the worldlist, like spring etc')
+                    help='used to add all season to the wordlist')
+parser.add_argument('-dutch', '--dutch', action='store_true',
+                    help='used to translate months and seasons to dutch')
 parser.add_argument('-aY', '--years', nargs='+', action='store', type=int, default=[], 
-                    help='used to append a year range to every word in the wordlist, seperate them by spaces for example 2018 2020')
-parser.add_argument('-f', '--file', action='store', type=str,
-                    help='used to supply a file with words to be added as input for the wordlist')
+                    help='used to append a range of years to every word in the wordlist, seperated by spaces for example 2018 2020')
 parser.add_argument('-AS', '--asymbols', action='store_true',
-                    help='used to append symbols to every word in the wordlist')
+                    help='used to append symbols to every word in the wordlist, the list of symbols can be edited in the code')
 parser.add_argument('-As', '--astrings', nargs='+', action='store', type=str, default=[],
                     help='used to append strings to every word in the wordlist before the symbols are applied')
 parser.add_argument('-PS', '--psymbols', action='store_true',
-                    help='used to prepend symbols to every word in the wordlist')
+                    help='used to prepend symbols to every word in the wordlist, the list of symbols can be edited in the code')
 parser.add_argument('-Ps', '--pstrings', nargs='+', action='store', type=str, default=[],
                     help='used to prepend strings to every word in the wordlist before the symbols are applied')
 parser.add_argument('-C', '--capital', action='store_true', 
                     help='used to capitalize every first letter in the word')
 parser.add_argument('-min', '--min', action='store', type=int,
-                    help='used to set the minimum password length')
+                    help='used to set the minimum password length of the outputted passwords')
 parser.add_argument('-max', '--max', action='store', type=int,
-                    help='used to set the maximum password length')
-parser.add_argument('-d', '--dutch', action='store_true', 
-                    help='used to translate months and seasons to dutch')
+                    help='used to set the maximum password length of the outputted passwords')
+parser.add_argument('-minc', '--minc', action='store_true',
+                    help='Set password requirement to have atleast a capital letter, removes every word that does not')
 parser.add_argument("-o", "--output", action='store', 
                     type=argparse.FileType('w'), dest='output',
                     help="Directs the output to a name of your choice")
@@ -45,11 +47,10 @@ seasons = ['spring', 'summer', 'autumn','fall' , 'winter']
 dutch_months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli',
           'augustus', 'september', 'oktober', 'november', 'december']
 dutch_seasons = ['lente', 'zomer', 'herfst', 'winter']
-symbols = ['!', '@', '.', '-', '_', '#', '$', '&', '+', '?', '123']
+symbols = ['.', '!', '?', '&', '@', '$', '#',]
+#symbols = ['.', '!', '?', '&', '-', '@', '_', '$', '*', '/', '#', '%', '^', '(', ')', '=', '+', '<', '>', '\', ',', ':', ';', '[', ']', '{', '}', '|', '123', '1234', 'qwe', 'qwerty', 'asd']
 wordlist = []
 newwordlist = []
-tempnewwordlist = []
-tempnewwordlist2 = []
 
 #print help message if no argument is supplied!
 if len(sys.argv)==1:
@@ -62,6 +63,12 @@ if args.dutch:
     months = dutch_months
     seasons = dutch_seasons
 
+if args.file:
+    print("[+] Adding words from", args.file, "to the wordlist")
+    with open(args.file, 'r') as f:
+        newwordlist = [line.strip() for line in f]
+        wordlist.extend(newwordlist)
+
 if args.months:
     print("[+] Adding months to the worldlist")
     wordlist.extend(months)
@@ -70,62 +77,70 @@ if args.seasons:
     print("[+] Adding seasons to the worldlist")
     wordlist.extend(seasons)
 
-if args.file:
-    with open(args.file, 'r') as f:
-        newworldlist = [line.strip() for line in f]
-        wordlist.extend(newwordlist)
-
-
 if args.strings != []:
     print("[+] Adding the provided strings to the worldlist")
     wordlist.extend(args.strings)
 
 if args.years != []:
     print("[+] Appending years to every word in the wordlist")
+    newwordlist = []
     for i in range(args.years[0], args.years[1]+1):
         yearwordlist = [x + str(i) for x in wordlist]
         newwordlist.extend(yearwordlist)
     wordlist.extend(newwordlist)
 
 if args.astrings != [] or args.pstrings != []:
+    newwordlist = []
     if args.astrings != []:
         print("[+] Appending strings to every word in the wordlist")
         for i in args.astrings:
             stringwordlist = [x + str(i) for x in wordlist]
-            tempnewwordlist.extend(stringwordlist)
+            newwordlist.extend(stringwordlist)
 
     if args.pstrings:
         print("[+] Prepending strings to every word in the wordlist")
         for i in args.pstrings:
             stringwordlist = [str(i) + x for x in wordlist]
-            tempnewwordlist2.extend(stringwordlist)
-    wordlist.extend(tempnewwordlist)
-    wordlist.extend(tempnewwordlist2)
+            newwordlist.extend(stringwordlist)
+    wordlist.extend(newwordlist)
 
 if args.asymbols or args.psymbols:
+    newwordlist = []
     if args.asymbols:
         print("[+] Appending symbols to every word in the wordlist")
         for i in symbols:
             symbolwordlist = [x + str(i) for x in wordlist]
-            tempnewwordlist.extend(symbolwordlist)
+            newwordlist.extend(symbolwordlist)
 
     if args.psymbols:
         print("[+] Prepending symbols to every word in the wordlist")
         for i in symbols:
             symbolwordlist = [str(i) + x for x in wordlist]
-            tempnewwordlist2.extend(symbolwordlist)
-    wordlist.extend(tempnewwordlist)
-    wordlist.extend(tempnewwordlist2)
+            newwordlist.extend(symbolwordlist)
+    wordlist.extend(newwordlist)
 
 if args.capital:
+    newwordlist = []
     print("[+] Capitalizing the first letter of every word")
     newwordlist = [words.capitalize() for words in wordlist]
     wordlist.extend(newwordlist)
 
-if args.min and args.max:
+if args.min or args.max:
     print("[-] Deleting words if smaller then", args.min, "characters and larger then", args.max,"characters")
-    for word in wordlist:
+    newwordlist = wordlist.copy()
+    for word in newwordlist:
         if (len(word) < args.min) or (len(word) > args.max): wordlist.remove(word)
+
+if args.minc:
+    print("[-] Deleting words that do not contain a capital character")
+    newwordlist = wordlist.copy()
+    for word in newwordlist:
+        if not any(x.isupper() for x in word):
+            wordlist.remove(word)
+    
+
+#Filter duplicates but keep order
+wordlist = sorted(set(wordlist))
 
 if args.output:
     output_file = args.output
